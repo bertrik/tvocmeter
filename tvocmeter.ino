246816da726ea30c7900f138de7be3763ea87c21
Bertrik Sikken <bertrik@sikken.nl>
2019-07-13 01:46:18 +0200

    Simplify things, remove the LED display, change to revspace host.

--- tvocmeter.ino
+++ tvocmeter.ino
@@ -6,27 +6,20 @@
 #include <SparkFunBME280.h>
 #include <WiFiManager.h>
 #include <PubSubClient.h>
-#include "FastLED.h"
-
-#define PIN_LED_VCC     D2
-#define PIN_LED_DATA    D3
 
 #define PIN_CCS811_WAK  D5
 #define PIN_CCS811_SDA  D6
 #define PIN_CCS811_SCL  D7
-#define PIN_CCS811_GND  D8
 
 #define CCS811_ADDR 0x5A
 
-#define MQTT_HOST   "stofradar.nl"
+#define MQTT_HOST   "mosquitto.space.revspace.nl"
 #define MQTT_PORT   1883
-#define MQTT_TOPIC  "bertrik/%s%s"      // esp_id/subtopic
+#define MQTT_TOPIC  "revspace/sensors/tvoc/%s/%s"       // esp_id/subtopic
 
 #define BASELINE_PERIOD_SEC 3600
 #define ENV_PERIOD_SEC      10
 #define LOG_PERIOD_SEC      10
-
-#define NUM_LEDS 8
 
 #define NVDATA_MAGIC 0x1337
 struct {
@@ -35,26 +28,9 @@ struct {
 } nvdata;
 
 typedef struct {
-    int level;
-    CRGB rgb;
-} level_t;
-
-typedef struct {
     uint32_t total;
     uint16_t count;
 } averager_t;
-
-static const level_t levels[] = {
-    {62,    CRGB::Green},
-    {125,   CRGB::Green},
-    {250,   CRGB::Green},
-    {500,   CRGB::Green},
-    {1000,  CRGB::Yellow},
-    {2000,  CRGB::Yellow},
-    {4000,  CRGB::Red},
-    {8000,  CRGB::Red},
-    {-1, 0}
-};
 
 static CCS811 ccs811(CCS811_ADDR);
 static BME280 bme280;
@@ -63,7 +39,7 @@ static char esp_id[16];
 static WiFiManager wifiManager;
 static WiFiClient wifiClient;
 static PubSubClient mqttClient(wifiClient);
-static CRGB leds[NUM_LEDS];
+static char statustopic[128];
 
 // printf-like output to serial port
 static void print(const char *fmt, ...)
@@ -71,9 +47,9 @@ static void print(const char *fmt, ...)
     // format it
     char buf[256];
     va_list args;
-    va_start (args, fmt);
+    va_start(args, fmt);
     vsnprintf(buf, sizeof(buf), fmt, args);
-    va_end (args);
+    va_end(args);
 
     // send it to serial
     Serial.write(buf);
@@ -81,18 +57,18 @@ static void print(const char *fmt, ...)
 
 void setup(void)
 {
-    // setup pins
-    pinMode(PIN_CCS811_GND, OUTPUT);
-    digitalWrite(PIN_CCS811_GND, 0);
-    pinMode(PIN_CCS811_WAK, OUTPUT);
-    digitalWrite(PIN_CCS811_WAK, 1);
+    delay(3000);
 
     // greeting
     Serial.begin(115200);
     print("\nTVOC meter\n");
 
+    // setup pins
+    pinMode(PIN_CCS811_WAK, OUTPUT);
+    digitalWrite(PIN_CCS811_WAK, 1);
+
     // get ESP id
-    sprintf(esp_id, "%08X", ESP.getChipId());
+    sprintf(esp_id, "%06X", ESP.getChipId());
     print("ESP ID: %s\n", esp_id);
 
     // setup I2C
@@ -101,16 +77,20 @@ void setup(void)
     // setup BME280
     bme280.setI2CAddress(0x76);
     if (!bme280.beginI2C()) {
-        print("bme280.begin() returned with an error.\n");
-        while (1);
+        while (1) {
+            print("bme280.begin() returned with an error.\n");
+            delay(1000);
+        }
     }
 
     // setup CCS811
     digitalWrite(PIN_CCS811_WAK, 0);
     CCS811Core::status returnCode = ccs811.begin();
     if (returnCode != CCS811Core::SENSOR_SUCCESS) {
-        print("ccs811.begin() returned with an error.\n");
-        while (1);
+        while (1) {
+            print("ccs811.begin() returned with an error.\n");
+            delay(1000);
+        }
     }
 
     // restore CCS811 baseline
@@ -121,25 +101,23 @@ void setup(void)
         ccs811.setBaseline(nvdata.baseline);
     }
 
-    // LED init
-    pinMode(PIN_LED_VCC, OUTPUT);
-    digitalWrite(PIN_LED_VCC, 1);
-    FastLED.addLeds<WS2812, PIN_LED_DATA, RGB>(leds, NUM_LEDS);
-    FastLED.setBrightness(5);
-    FastLED.show();
-
     // connect to wifi
     print("Starting WIFI manager ...\n");
     wifiManager.setConfigPortalTimeout(120);
     wifiManager.autoConnect("ESP-TVOC");
+
+    snprintf(statustopic, sizeof(statustopic), MQTT_TOPIC, esp_id, "status");
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 }
 
 static bool mqtt_send(const char *topic, const char *value, bool retained)
 {
     bool result = false;
     if (!mqttClient.connected()) {
-        mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-        result = mqttClient.connect(esp_id, topic, 0, retained, "offline");
+        result = mqttClient.connect(esp_id, statustopic, 0, retained, "offline");
+        if (result) {
+            result = mqttClient.publish(statustopic, "online", retained);
+        }
     }
     if (mqttClient.connected()) {
         print("Publishing %s to %s ...", value, topic);
@@ -150,31 +128,14 @@ static bool mqtt_send(const char *topic, const char *value, bool retained)
     return result;
 }
 
-static void show_on_led(uint16_t tvoc)
-{
-    int idx = 0;
-    for (idx = 0; levels[idx].level > 0; idx++) {
-        const level_t *l = &levels[idx];
-        if (tvoc >= l->level) {
-            leds[idx] = l->rgb;
-            print("#");
-        } else {
-            leds[idx] = CRGB::Black; 
-            print(".");
-        }
-    }
-    print(" %d\n", tvoc);
-    FastLED.show(); 
-}
-
 void loop(void)
 {
     static unsigned long second_log = 0;
     static unsigned long second_baseline = 0;
     static unsigned long second_env = 0;
 
-    static averager_t tvoc_avg = {0, 0};
-    static averager_t eco2_avg = {0, 0};
+    static averager_t tvoc_avg = { 0, 0 };
+    static averager_t eco2_avg = { 0, 0 };
 
     char topic[128];
     char message[16];
@@ -188,9 +149,6 @@ void loop(void)
         uint16_t eco2 = ccs811.getCO2();
         eco2_avg.total += eco2;
         eco2_avg.count++;
-
-        // update LEDs
-        show_on_led(tvoc);
     }
 
     // save CCS811 baseline every BASELINE_PERIOD_SEC
@@ -218,13 +176,13 @@ void loop(void)
         digitalWrite(PIN_CCS811_WAK, 0);
         print("Applying T/RH compensation: T=%.2f, RH=%.2f\n", tempC, humidity);
         ccs811.setEnvironmentalData(humidity, tempC);
-    
+
         // log to MQTT
-        snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "/bme280/temperature");
+        snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "bme280/temperature");
         snprintf(message, sizeof(message), "%.2f", tempC);
         mqtt_send(topic, message, true);
 
-        snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "/bme280/humidity");
+        snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "bme280/humidity");
         snprintf(message, sizeof(message), "%.2f", humidity);
         mqtt_send(topic, message, true);
     }
@@ -235,24 +193,24 @@ void loop(void)
 
         // calculate average TVOC
         if (tvoc_avg.count > 0) {
-            uint16_t tvoc = (tvoc_avg.total + (tvoc_avg.count / 2)) / tvoc_avg.count; 
+            uint16_t tvoc = (tvoc_avg.total + (tvoc_avg.count / 2)) / tvoc_avg.count;
             tvoc_avg.total = 0;
             tvoc_avg.count = 0;
 
             // send over MQTT
-            snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "/ccs811/tvoc");
+            snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "ccs811/tvoc");
             snprintf(message, sizeof(message), "%u", tvoc);
             mqtt_send(topic, message, true);
         }
 
         // calculate average eCO2
         if (eco2_avg.count > 0) {
-            uint16_t eco2 = (eco2_avg.total + (eco2_avg.count / 2)) / eco2_avg.count; 
+            uint16_t eco2 = (eco2_avg.total + (eco2_avg.count / 2)) / eco2_avg.count;
             eco2_avg.total = 0;
             eco2_avg.count = 0;
 
             // send over MQTT
-            snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "/ccs811/eco2");
+            snprintf(topic, sizeof(topic), MQTT_TOPIC, esp_id, "ccs811/eco2");
             snprintf(message, sizeof(message), "%u", eco2);
             mqtt_send(topic, message, true);
         }
@@ -267,4 +225,3 @@ void loop(void)
         ESP.restart();
     }
 }
-
